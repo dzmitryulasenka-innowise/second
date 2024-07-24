e31e0ba77c80207b3b184c11ad3a96b618c10e28
dzmitryulasenka <dzmitry.ulasenka@innowise.com>
2024-07-24 08:56:50 +0300

    add working pagination

--- wp-content/themes/mytheme/archive-product.php
+++ wp-content/themes/mytheme/archive-product.php
@@ -1,61 +1,135 @@
 <?php get_header(); ?>
 
+<?php
+// Создаем WP_Query с нужными аргументами
+//$args = array(
+//    'post_type' => 'product',
+//    'posts_per_page' => 5,
+//    'paged' => get_query_var( 'page' ) // Используем 'paged' для текущей страницы
+//);
+
+$query = new WP_Query([
+    'post_type' => 'product',
+    'posts_per_page' => 8,
+    'paged' => get_query_var('page') ?: 1
+]);
+
+//$query = new WP_Query( $args );
+
+
+
+if ( $query->have_posts() ) : ?>
     <div class="container">
-        <h1 class="archive-title"><?php the_archive_title(); ?></h1>
-        <?php if ( have_posts() ) : ?>
+        <h1 class="archive-title"><?php the_archive_title();        ?></h1>
+        <div class="row">
 
-            <div class="container">
-                <div class="row">
+            <?php while ( $query->have_posts() ) : $query->the_post(); ?>
 
-                    <div class="col-md-3 mb-4">
-                        <div class="card">
-                            <img src="https://via.placeholder.com/300x200" class="card-img-top" alt="Товар 1">
-                            <div class="card-body">
-                                <h5 class="card-title">Товар 1</h5>
-                                <p class="card-text">Описание товара 1.</p>
-                                <a href="#" class="btn btn-primary">Подробнее</a>
-                            </div>
+                <div class="col-md-3 mb-4">
+                    <article <?php post_class(); ?>>
+                        <small><?php the_time('F jS, Y') ?> Автор: <?php the_author_posts_link() ?></small>
+
+                        <div>
+                            <?php the_post_thumbnail('thumbnail'); ?>
                         </div>
-                    </div>
 
+                        <div>
+                            <p> Price: <?php the_field('price'); ?></p>
+                            <p> Count: <?php the_field('count'); ?></p>
+                        </div>
+
+                        <div>
+                            <a href="<?php the_permalink(); ?>" class="btn btn-primary">Купить</a>
+                        </div>
+                    </article>
                 </div>
-            </div>
+
+            <?php endwhile;
+            $GLOBALS['wp_query'] = $query;?>
 
 
+            <?php the_posts_pagination(
+                [
+                    'format' => '?page=%#%',
+                    'prev_text' => 'Previous page',
+                    'next_text' => 'Next page',
+                    'before_page_number' => '<span class="meta-nav screen-reader-text">Page</span>'
+                ]);
+            ?>
 
-
-
-            <?php while ( have_posts() ) : the_post(); ?>
-
-                <article <?php post_class(); ?>>
-                    <!-- Выводим дату поста и ссылку на другие записи автора. -->
-                    <small><?php the_time('F jS, Y') ?> Автор: <?php the_author_posts_link() ?></small>
-
-                    <div>
-                        <!--                        --><?php //if (has_post_thumbnail()) : ?>
-                        <?php the_post_thumbnail('thumbnail'); ?>
-                        <!--                        --><?php //endif; ?>
-                    </div>
-
-                    <div>
-                        <!--                        --><?php //if (the_field('price')) : ?>
-                        <p> Price: <?php the_field('price'); ?></p>
-                        <!--                        --><?php //endif; ?>
-
-                        <!--                        --><?php //if (the_field('count')) : ?>
-                        <p> Count: <?php the_field('count'); ?></p>
-                        <!--                        --><?php //endif; ?>
-                    </div>
-
-                    <div>
-                        <a href="<?php the_permalink(); ?>" class="btn btn-primary">Купить</a>
-                    </div>
-
-                </article>
-
-            <?php endwhile; ?>
-        <?php else : ?>
-            <p><?php esc_html_e( 'Sorry, no posts matched your criteria.' ); ?></p>
-        <?php endif; ?>
+        </div>
     </div>
+<?php else : ?>
+    <p><?php esc_html_e('Sorry, no posts matched your criteria.'); ?></p>
+<?php endif; ?>
+<?php wp_reset_postdata(); ?>
+
 <?php get_footer(); ?>
+
+
+
+
+
+<?php
+//
+//get_header(); ?>
+<!---->
+<!--<div id="container w-100  ">-->
+<!--    <div class="row w-100">-->
+<!--        <div class="col-10">-->
+<!---->
+<!--            --><?php
+//
+//            $query = new WP_Query([
+//                'post_type' => 'product',
+//                'posts_per_page' => 8,
+//                'paged' => get_query_var('page') ?: 1
+//            ]);
+//
+//            if ($query->have_posts()) : ?>
+<!--                <div class="row w-100">-->
+<!--                    --><?php //while ($query->have_posts()) : $query->the_post() ?>
+<!--                        --><?php //?>
+<!--                        <div class="card col-3">-->
+<!--                            --><?php //the_post_thumbnail('thumbnail', ['class' => 'card-image']); ?>
+<!--                            <div class="card-body">-->
+<!--                                <h5 class="card-title">--><?php //echo get_the_title() ?><!--</h5>-->
+<!--                                <p class="card-text">--><?php //the_excerpt() ?><!--</p>-->
+<!--                                <p class="">--><?php //echo get_field('price') ?><!--</p>-->
+<!--                                <div>-->
+<!--                                    <a href="--><?php //echo get_the_permalink() ?><!--" class="btn btn-primary">Купить</a>-->
+<!--                                </div>-->
+<!--                            </div>-->
+<!--                        </div>-->
+<!--                    --><?php //endwhile;
+//
+//                    $GLOBALS['wp_query'] = $query;
+//                    ?>
+<!--                    <div class="row w-100">-->
+<!--                        --><?php //the_posts_pagination(
+//                            [
+//                                'format' => '?page=%#%',
+//                                'prev_text' => 'Previous page',
+//                                'next_text' => 'Next page',
+//                                'before_page_number' => '<span class="meta-nav screen-reader-text">Page</span>'
+//                            ]);
+//                        ?>
+<!--                    </div>-->
+<!--                    --><?php
+//                    wp_reset_postdata(); ?>
+<!--                </div>-->
+<!---->
+<!--            --><?php //else :
+//                echo '<p>No posts found in this category.</p>';
+//            endif;
+//
+//
+//            ?>
+<!--        </div>-->
+<!--        <div class="col-2">-->
+<!--            --><?php //dynamic_sidebar('rating-sidebar'); ?>
+<!--        </div>-->
+<!--    </div>-->
+<!--</div>-->
+<!---->
+<?php //get_footer(); ?>
--- wp-content/themes/mytheme/functions.php
+++ wp-content/themes/mytheme/functions.php
@@ -49,29 +49,97 @@ add_action( 'after_setup_theme', 'register_navwalker' );
 
 
 function create_product_post_type() {
-    register_post_type( 'product',
-        array(
-            'labels' => array(
-                'name' => __( 'Products', 'textdomain' ),
-                'singular_name' => __( 'Product', 'textdomain' ),
-                'add_new' => __( 'Add new', 'textdomain' ),
-                'add_new_item' => __( 'Add new product', 'textdomain' ),
-                'edit_item' => __( 'Edit product', 'textdomain' ),
-                'new_item' => __( 'New product', 'textdomain' ),
-                'view_item' => __( 'View product', 'textdomain' ),
-                'search_items' => __( 'Search product', 'textdomain' ),
-                'not_found' => __( 'Products not found', 'textdomain' ),
-                'not_found_in_trash' => __( 'Products not found in trash', 'textdomain' ),
-                'parent_item_colon' => __( 'Parent item:', 'textdomain' ),
-                'menu_name' => __( 'Products', 'textdomain' ),
-            ),
-            'public' => true,
-            'has_archive' => true,
-            'show_in_nav_menus' => true,
-        )
-    );
+    register_post_type('product', [
+        'label' => null,
+        'labels' => [
+            'name' => 'Products', // основное название для типа записи
+            'singular_name' => 'Product', // название для одной записи этого типа
+            'add_new' => 'Add product', // для добавления новой записи
+            'add_new_item' => 'Add product', // заголовка у вновь создаваемой записи в админ-панели.
+            'edit_item' => 'Add product', // для редактирования типа записи
+            'new_item' => 'New product', // текст новой записи
+            'view_item' => 'View product', // для просмотра записи этого типа.
+            'search_items' => 'Search product', // для поиска по этим типам записи
+            'not_found' => 'Not found', // если в результате поиска ничего не было найдено
+            'not_found_in_trash' => 'Not found in trash', // если не было найдено в корзине
+            'parent_item_colon' => '', // для родителей (у древовидных типов)
+            'menu_name' => 'Product', // название меню
+        ],
+        'description' => 'product description',
+        'public' => true,
+        // 'publicly_queryable'  => null, // зависит от public
+        // 'exclude_from_search' => null, // зависит от public
+        // 'show_ui'             => null, // зависит от public
+        // 'show_in_nav_menus'   => null, // зависит от public
+        'show_in_menu' => true, // показывать ли в меню админки
+        // 'show_in_admin_bar'   => null, // зависит от show_in_menu
+        'show_in_rest' => null, // добавить в REST API. C WP 4.7
+        'rest_base' => null, // $post_type. C WP 4.7
+        'menu_position' => null,
+        'menu_icon' => null,
+        //'capability_type'   => 'post',
+        //'capabilities'      => 'post', // массив дополнительных прав для этого типа записи
+        //'map_meta_cap'      => null, // Ставим true чтобы включить дефолтный обработчик специальных прав
+        'hierarchical' => false,
+        'supports' => ['title', 'editor', 'thumbnail', 'comments'], // 'title','editor','author','thumbnail','excerpt','trackbacks','custom-fields','comments','revisions','page-attributes','post-formats'
+        'taxonomies' => [],
+        'has_archive' => true,
+        'query_var' => true,
+    ]);
 }
 add_action( 'init', 'create_product_post_type' );
 
 
 add_theme_support( 'post-thumbnails');
+
+
+
+
+//function pixelnet_bootstrap_pagination( $wp_query = false, $echo = true, $args = array() ) {
+//    //Fallback to $wp_query global variable if no query passed
+//    if ( false === $wp_query ) {
+//        global $wp_query;
+//    }
+//
+//    //Set Defaults
+//    $defaults = array(
+//        'base'         => str_replace( 999999999, '%#%', esc_url( get_pagenum_link( 999999999 ) ) ),
+//        'format'       => '?paged=%#%',
+//        'current'      => max( 1, get_query_var( 'page' ) ),
+//        'total'        => $wp_query->max_num_pages,
+//        'type'         => 'array',
+//        'show_all'     => false,
+//        'end_size'     => 2,
+//        'mid_size'     => 1,
+//        'prev_text'    => __( '« Prev' ),
+//        'next_text'    => __( 'Next »' ),
+//        'add_fragment' => '',
+//    );
+//
+//    //Merge the defaults with passed arguments
+//    $merged = wp_parse_args( $args, $defaults );
+//    print_r($merged);
+//    //Get the paginated links
+//    $lists = paginate_links($merged);
+//    var_dump($lists);
+////    print_r('qweqweq');
+//    if ( is_array( $lists ) ) {
+//
+//        $html = '<nav><ul class="pagination justify-content-center">';
+//
+//        foreach ( $lists as $list ) {
+//
+//            $html .= '<li class="page-item' . (strpos($list, 'current') !== false ? ' active' : '') . '"> ' . str_replace('page-numbers', 'page-link text-dark', $list) . '</li>';
+//        }
+//
+//        $html .= '</ul></nav>';
+//
+//        if ( $echo ) {
+//            echo $html;
+//        } else {
+//            return $html;
+//        }
+//    }
+//
+//    return false;
+//}
